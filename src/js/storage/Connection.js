189d2ecbf8d9ab2f1d8897871aabbf6225c65d35
mariof99 <marioferestevez@gmail.com>
2022-11-07 18:54:56 +0100

    search characters function

---
+++ src/js/storage/Connection.js
@@ -0,0 +1,29 @@
+import {movies, people} from "./data";
+
+export const searchMovie = async(search) => {
+    const movieSearch = movies.filter(mov => {
+        return mov.title.toUpperCase().includes(search.trim().toUpperCase());
+    });
+
+    if (movieSearch.length > 0) {
+        return movieSearch;
+    }
+    else {
+        throw Error('could not find the movie');
+    }
+}
+
+export const searchCharacters = async(movieId) => {
+    const movieCharacters = people.filter(char => {
+        return char.films[0] == movieId;
+    });
+
+    if (movieCharacters.length > 0) {
+        return movieCharacters;
+    }
+    else {
+        throw Error('could not find the characters');
+    }
+}
+
+
