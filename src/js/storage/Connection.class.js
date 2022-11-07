51511bf63f9b4e2c941c8ad4bd2e9ee91b1d469c
mariof99 <marioferestevez@gmail.com>
2022-11-07 17:46:55 +0100

    search movie function

--- src/js/storage/Connection.class.js
+++ src/js/storage/Connection.class.js
@@ -1,19 +1,22 @@
 import {movies} from "./data";
 
 export class Connection {
-    static searchMovie = async(id) => {
-        const movie = movies.filter(mov => {
-            return mov.id === id;
+    static searchMovie = async(search) => {
+        const movieSearch = movies.filter(mov => {
+            return mov.title.toUpperCase().includes(search.trim().toUpperCase());
         });
 
-        if (movie) {
-            return movie;
+        if (movieSearch.length > 0) {
+            return movieSearch;
         }
         else {
             throw Error('could not find the movie');
         }
     }
+
+    static searchCharacters = async(characters) => {
+
+    }
 }
 
 console.log(Connection.searchMovie('12cfb892-aac0-4c5b-94af-521852e46d6a'));
-
